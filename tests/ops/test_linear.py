396b3835d7ff9a6f0a4d4eb8a1e7fc5c91cc6e69
AnihilatorGun <nikitaushakov.nu35@gmail.com>
2025-01-14 18:46:25 +0300

    cleaner linear test

--- tests/ops/test_linear.py
+++ tests/ops/test_linear.py
@@ -85,36 +85,36 @@ def test_linbrelulinadd(bsize, channels, other_1, other_2, other_3):
         memory_format=torch.channels_last_3d
     )
     add_other = torch.randn_like(x)
-    weight_up = (
-        nn.Conv3d(channels, 2 * channels, 1, bias=False, device='cuda')
-        .weight[:, :, 0, 0, 0]
-        .permute(1, 0)
-        .contiguous()
-        .to(torch.float16)
-    )
-    weight_down = (
-        nn.Conv3d(2 * channels, channels, 1, bias=False, device='cuda')
-        .weight[:, :, 0, 0, 0]
-        .permute(1, 0)
-        .contiguous()
-        .to(torch.float16)
-    )
-    bias = torch.randn(2 * channels, dtype=torch.float16, device='cuda')
 
-    out = LinBReLULinAdd(x, weight_up, weight_down, bias, add_other)
+    weight_up = torch.randn(channels, channels * 2, dtype=torch.float32, device='cuda')
+    nn.init.kaiming_uniform_(weight_up, a=math.sqrt(5))
 
-    out_base = F.conv3d(x, weight_up.permute(1, 0)[:, :, None, None, None], bias)
-    out_base = F.relu(out_base)
-    out_base = F.conv3d(out_base, weight_down.permute(1, 0)[:, :, None, None, None], None)
-    out_base += add_other
+    weight_down = torch.randn(channels * 2, channels, dtype=torch.float32, device='cuda')
+    nn.init.kaiming_uniform_(weight_down, a=math.sqrt(5))
+
+    bias = torch.randn(2 * channels, dtype=torch.float32, device='cuda')
+
+    out = LinBReLULinAdd(
+        x,
+        weight_up.to(torch.float16),
+        weight_down.to(torch.float16),
+        bias.to(torch.float16),
+        add_other
+    )
+
+    with torch.amp.autocast('cuda'), torch.inference_mode(): 
+        out_base = F.conv3d(x, weight_up.permute(1, 0)[:, :, None, None, None], bias)
+        out_base = F.relu(out_base)
+        out_base = F.conv3d(out_base, weight_down.permute(1, 0)[:, :, None, None, None], None)
+        out_base += add_other
 
     assert allclose_two_stage(
         out_base,
         out,
         rtol_strict=1e-4,
         atol_strict=5e-4,
-        rtol_narrow=1e-4,
-        atol_narrow=2e-3,
+        rtol_narrow=1e-3,
+        atol_narrow=1e-3,
         debug_info='print',
     )
 
