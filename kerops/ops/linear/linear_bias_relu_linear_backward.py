LinbReLULinBackward now produces FP16 gradients, test fix

kerops/ops/linear/linear_bias_relu_linear_backward.py
@@ -46,9 +46,9 @@ def LinBReLULinBackward(
     grid_size = ceil(numel_no_channels / (D_block * _ILP))
 
     x_grad = torch.empty_like(x)
-    weight_up_grad = torch.zeros([grid_size, in_channels, hidden_channels], dtype=torch.float32, device='cuda')
-    weight_down_grad = torch.zeros([grid_size, hidden_channels, in_channels], dtype=torch.float32, device='cuda')
-    bias_grad = torch.zeros([grid_size, hidden_channels], dtype=torch.float32, device='cuda')
+    weight_up_grad = torch.zeros([grid_size, in_channels, hidden_channels], dtype=torch.float16, device='cuda')
+    weight_down_grad = torch.zeros([grid_size, hidden_channels, in_channels], dtype=torch.float16, device='cuda')
+    bias_grad = torch.zeros([grid_size, hidden_channels], dtype=torch.float16, device='cuda')
 
     _LinBReLULinBackward[(grid_size,)](
         x,

tests/ops/test_linear.py
@@ -149,6 +149,10 @@ def test_linbrelulin_backward(bsize, channels, other_1, other_2, other_3):
         bias.to(torch.float16),
     )
 
+    weight_up_grad = weight_up_grad.to(torch.float32)
+    weight_down_grad = weight_down_grad.to(torch.float32)
+    bias_grad = bias_grad.to(torch.float32)
+
     with torch.amp.autocast('cuda'):
         x_ = F.conv3d(x, weight_up.permute(1, 0)[:, :, None, None, None], bias, stride=1, padding=0)
         x_ = F.relu(x_)
@@ -177,8 +181,8 @@ def test_linbrelulin_backward(bsize, channels, other_1, other_2, other_3):
     )
 
     assert weight_grad_similarity(
-        weight_up.grad,
-        weight_up_grad,
+        bias.grad,
+        bias_grad,
         rtol_cos=1e-3,
         atol_cos=1e-3,
         rtol_len=2e-3 * bsize,